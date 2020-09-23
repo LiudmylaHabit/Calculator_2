44f27db9ed3a674537885a043cf5a182b4dbb834
vohony1 <vohony1@gmail.com>
2020-09-23 18:16:25 +0300

    Implement tests for polish reader method

--- UnitTestProject1/CalculationTests.cs
+++ UnitTestProject1/CalculationTests.cs
@@ -13,27 +13,27 @@ namespace UnitTestProject1
         [TestCase(-1, -1)]
         [TestCase(1.25, 0.8)]
         [TestCase(3, 0.3333)]
-        public void DivisionOnOneChecking(decimal a, decimal expectedResult)
+        public void DivisionOnOneChecking(decimal operand, decimal expectedResult)
         {
-            decimal result = _calc.OneDivision(a);
+            decimal result = _calc.OneDivision(operand);
             Assert.AreEqual(expectedResult, result);
         }
 
         [TestCase(0, 10, 10)]
         [TestCase(0, -10, -10)]
         [TestCase(3.4, 2.265, 5.665)]
-        public void AddChecking(decimal a, decimal b, decimal expectedResult)
+        public void AddChecking(decimal firstOperand, decimal secondOperand, decimal expectedResult)
         {
-            decimal result = _calc.Add(a, b);
+            decimal result = _calc.Add(firstOperand, secondOperand);
             Assert.AreEqual(expectedResult, result);
         }
 
         [TestCase(0, 10, -10)]
         [TestCase(0, -10, 10)]
         [TestCase(3.4002, 2.265, 1.1352)]
-        public void SubstractChecking(decimal a, decimal b, decimal expectedResult)
+        public void SubstractChecking(decimal firstOperand, decimal secondOperand, decimal expectedResult)
         {
-            decimal result = _calc.Substract(a, b);
+            decimal result = _calc.Substract(firstOperand, secondOperand);
             Assert.AreEqual(expectedResult, result);
         }
 
@@ -41,29 +41,52 @@ namespace UnitTestProject1
         [TestCase(0, -10, 0)]
         [TestCase(3, -10, -30)]
         [TestCase(145, 0.5, 72.5)]
-        public void MultiplyChecking(decimal a, decimal b, decimal expectedResult)
+        public void MultiplyChecking(decimal firstOperand, decimal secondOperand, decimal expectedResult)
         {
-            decimal result = _calc.Multiply(a, b);
+            decimal result = _calc.Multiply(firstOperand, secondOperand);
             Assert.AreEqual(expectedResult, result);
         }
-
 
         [TestCase(5, 10, 0.5)]
         [TestCase(0, 10, 0)]
         [TestCase(0, 0, 0)]
         [TestCase(3.4, 0, 0)]
-        public void DivisionChecking(decimal a, decimal b, decimal expectedResult)
+        public void DivisionChecking(decimal firstOperand, decimal secondOperand, decimal expectedResult)
         {
-            decimal result = _calc.Divide(a, b);
+            decimal result = _calc.Divide(firstOperand, secondOperand);
             Assert.AreEqual(expectedResult, result);
         }
 
         [TestCase(100, 3, 1)]
         [TestCase(100, 1, 0)]
-        public void ReminderAfterDivisionChecking(decimal a, decimal b, decimal expectedResult)
+        public void ReminderAfterDivisionChecking(decimal firstOperand, decimal secondOperand, decimal expectedResult)
         {
-            decimal result = _calc.DivisionReminder(a, b);
+            decimal result = _calc.DivisionReminder(firstOperand, secondOperand);
             Assert.AreEqual(expectedResult, result);
         }
+
+        [Test (Description = "Check correctness of Polish Reader method ")]
+        public void PolishReaderExampleChecking()
+        {
+            decimal result = _calc.PolishReader("3+4*2/(1-5)^2");
+            Assert.AreEqual(3.5, result);
+        }
+
+        [TestCase("2+6", 8, Description = "Check correctness of Polish Reader method")]
+        [TestCase("0-2", -2)]
+        [TestCase("5*0", 0)]
+        [TestCase("6/2", 3)]
+        [TestCase("1+(3-4)", 0)]
+        [TestCase("2*(2+5)-3^2", 5)]
+        [TestCase("(2+5-3+10)*1", 7)]
+        [TestCase("3+4*2/(1-5)^2", 3.5)]
+        [TestCase("++1", 0)]
+        [TestCase("3++", 0)]
+        public void PolishReaderCustomChecking(string expression, decimal expectedResult)
+        {
+            decimal result = _calc.PolishReader(expression);
+            Assert.AreEqual(expectedResult, result);
+        }
+
     }
 }
